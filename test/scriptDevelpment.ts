82ee40370b621448c67a9cae4bdb151143f8a6dc
Tranquil Flow <tranquil_flow@protonmail.com>
2022-05-25 22:29:12 -0400

    minor additions, add DEX + base ERC20 contract

--- test/scriptDevelpment.ts
+++ test/scriptDevelpment.ts
@@ -1,13 +1,30 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { TestToken as TestTokenType,
+        DEXFactory as DEXFactoryType,
+        DEXRouter as DEXRouterType}
+        from "../typechain";
 
+let owner: SignerWithAddress;
+let addr1: SignerWithAddress;
+let addr2: SignerWithAddress;
+let addr3: SignerWithAddress;
+let addr4: SignerWithAddress;
+let addr5: SignerWithAddress;
+let addr6: SignerWithAddress;
+let addr7: SignerWithAddress;
+let addr8: SignerWithAddress;
+let addr9: SignerWithAddress;
+let addr10: SignerWithAddress;
 
 // import DEX router
 // import token
 // import backing token (USDC)
 
 // setup 10 addr
-// give each addr 100000 tokens so they can all sell without running out (more elegant way?)
+// give each addr 1% of token supply so they can all sell without running out (more elegant way?)
+// add liquidity to DEX (100,000 USDC + 50% supply default)
 
 ////////////////// BATCHES //////////////////
 
@@ -46,19 +63,42 @@ import { ethers } from "hardhat";
 
 /////////////////////////////////////////////
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("FlowTestingScripts", function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async function () {
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    [owner, addr1, addr2, addr3, addr4, addr5, addr6, addr7, addr8, addr9, addr10] = await ethers.getSigners();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    TestToken = await ethers.getContractFactory("TestToken");
+    testToken = await TestToken.deploy();
+    await testToken.deployed();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // DEXFactory = await ethers.getContractFactory("DEXFactory");
+    // dexFactory = await DEXFactory.deploy();
+    // await dexFactory.deployed();
+
+    // DEXRouter = await ethers.getContractFactory("DEXRouter");
+    // dexRouter = await DEXRouter.deploy();
+    // await dexRouter.deployed();
+
+    await testToken.mint(addr1, ethers.utils.parseEther("100")); // TO-DO mint % of supply instead of hard number
   });
+
 });
+
+
+
+/* HELPFUL CODE
+
+      // move time forwards
+      let oneDay = 86400; // seconds
+      await ethers.provider.send('evm_increaseTime', [oneDay * 98]); // Increase time 
+      await ethers.provider.send('evm_mine', []) // Force mine to update block timestamp
+
+      // call contract with specific address
+      await testToken.connect(addr1).FUNCTION([var1, var2]);
+
+      // call contract
+      await testToken.balanceOf(addr1.address)
+
+*/
