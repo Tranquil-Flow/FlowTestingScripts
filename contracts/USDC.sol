71aa6d80c0b3c26498d764960f209fc5d84482e3
Tranquil Flow <tranquil_flow@protonmail.com>
2022-06-15 23:47:59 -0400

    update progress

---
+++ contracts/USDC.sol
@@ -0,0 +1,1 @@
+//placeholder, TD: REPLACE
--- test/scriptDevelpment.ts
+++ test/scriptDevelpment.ts
@@ -1,10 +1,26 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { TestToken as TestTokenType,
+import { USDC as USDCType,
+        WETH as WETHType,
+        TestToken as TestTokenType,
         DEXFactory as DEXFactoryType,
-        DEXRouter as DEXRouterType}
-        from "../typechain";
+        DEXRouter as DEXRouterType,
+        DEXPair as DEXPairType
+      } from "../typechain";
+
+let USDC;
+let usdc: USDCType;
+let WETH;
+let weth: WETHType;
+let TestToken;
+let testToken: TestTokenType;
+let DEXFactory;
+let dexFactory: DEXFactoryType;
+let DEXRouter;
+let dexRouter: DEXRouterType;
+let DEXPair;
+let dexPair: DEXPairType;
 
 let owner: SignerWithAddress;
 let addr1: SignerWithAddress;
@@ -18,13 +34,63 @@ let addr8: SignerWithAddress;
 let addr9: SignerWithAddress;
 let addr10: SignerWithAddress;
 
-// import DEX router
-// import token
-// import backing token (USDC)
+describe("FlowTestingScripts", function () {
 
-// setup 10 addr
-// give each addr 1% of token supply so they can all sell without running out (more elegant way?)
-// add liquidity to DEX (100,000 USDC + 50% supply default)
+  // Inital Setup of Accounts
+  [owner, addr1, addr2, addr3, addr4, addr5, addr6, addr7, addr8, addr9, addr10] = await ethers.getSigners();
+
+  USDC = await ethers.getContractFactory("USDC");
+  usdc = await USDC.deploy();
+  await usdc.deployed();
+
+  WETH = await ethers.getContractFactory("WETH");
+  weth = await WETH.deploy();
+  await weth.deployed();
+
+  TestToken = await ethers.getContractFactory("TestToken");
+  testToken = await TestToken.deploy();
+  await testToken.deployed();
+
+  DEXFactory = await ethers.getContractFactory("DEXFactory");
+  dexFactory = await DEXFactory.deploy(owner);
+  await dexFactory.deployed();
+
+  // let factoryAddr = TD: GET DEPLOYED FACTORY ADDRESS
+  // let WETHAddr = TD: GET DEPLOYED WETH ADDRESS
+
+  DEXRouter = await ethers.getContractFactory("DEXRouter");
+  dexRouter = await DEXRouter.deploy(factoryAddr, WETHAddr);
+  await dexRouter.deployed();
+
+  let tokenSupply = testToken.totalSupply;
+  // let initialTransferAmount = tokenSupply / 10;
+  // let initialLiquidityAmount = tokenSupply / 2;
+
+  //TD: Mint 500,000 USDC to owner
+  // await usdc.connect(owner).?
+
+  //TD: USDC, usdc or new USDCAddr?
+  // Adds 500,000 USDC and 500,000 TEST
+  await dexRouter.connect(owner).addLiquidity([
+    usdc,
+    testToken,
+    500000,
+    500000,
+    0,
+    0,
+    owner,
+    //deadline
+    ]);  
+
+    // let pair = TD: GET DEPLOYED PAIR ADDR. Set to already DEXPair/dexPair?
+
+  // await testToken.connect(owner).transfer([addr1, initialTransferAmount]); // TD: transfer 5% of supply instead of hard number
+
+});
+
+
+
+
 
 ////////////////// BATCHES //////////////////
 
@@ -39,6 +105,14 @@ let addr10: SignerWithAddress;
 
 // buy transaction code
 //    buyValue
+
+await pair.connect(addr1).swap([
+  //amount0Out,
+  //amount1Out,
+  //to
+  //data
+])
+
 // sell transaction code
 //    sellValue
 
@@ -56,37 +130,26 @@ let addr10: SignerWithAddress;
 // buy, sell, sell, sell, sell, sell, sell, sell, sell, sell
 // sell, sell, sell, sell, sell, sell, sell, sell, sell, sell
 
+// batch in which all 10 trades are purchases
+describe("BatchB10", function () {
+
+});
+
+
+
 ////////////////// SCANNER //////////////////
 
 // console(log)
 // script to display all data:
 
-/////////////////////////////////////////////
-
-describe("FlowTestingScripts", function () {
-
-  beforeEach(async function () {
-
-    [owner, addr1, addr2, addr3, addr4, addr5, addr6, addr7, addr8, addr9, addr10] = await ethers.getSigners();
-
-    TestToken = await ethers.getContractFactory("TestToken");
-    testToken = await TestToken.deploy();
-    await testToken.deployed();
-
-    // DEXFactory = await ethers.getContractFactory("DEXFactory");
-    // dexFactory = await DEXFactory.deploy();
-    // await dexFactory.deployed();
-
-    // DEXRouter = await ethers.getContractFactory("DEXRouter");
-    // dexRouter = await DEXRouter.deploy();
-    // await dexRouter.deployed();
-
-    await testToken.mint(addr1, ethers.utils.parseEther("100")); // TO-DO mint % of supply instead of hard number
-  });
+describe("Scanner", function () {
 
 });
 
 
+
+
+/////////////////////////////////////////////
 
 /* HELPFUL CODE
 
